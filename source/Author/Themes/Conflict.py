74d9fd69b076523db941c7d8318f15473776c771
tyler.im <tji2105@columbia.edu>
2016-05-14 02:56:48 -0400

    uh

--- source/Author/Themes/Conflict.py
+++ source/Author/Themes/Conflict.py
@@ -1,18 +1,16 @@
 """
 @date: 5-14-16
 
-Description: Creates the conflict and broad outlines of its relations
+Description: Creates the conflict and broad outlines of story relations
 """
-
-import #relation
 
 class Conflict:
 
-    self.starting_relation
+    self.starting_relation = ""
 
-    self.resolution_relation
+    self.resolution_relation ""
 
-    self.final_relation
+    self.final_relation = ""
 
     self.relation_outlines = [self.starting_relation,
                               self.resolution_relation,
