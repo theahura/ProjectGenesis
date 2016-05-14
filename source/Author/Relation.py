d6bcec7328afee2c8b406dd4af84c56c620ca197
tyler.im <tji2105@columbia.edu>
2016-05-14 02:54:01 -0400

    set up conflict file

--- source/Author/Relation.py
+++ source/Author/Relation.py
@@ -3,3 +3,5 @@
 Description: Defines the relation class as <cluster> <relation term> <cluster>. Acts as the
 bread and butter of the author's development
 """
+
+class Relation:
---
+++ source/Author/Themes/Conflict.py
@@ -0,0 +1,21 @@
+"""
+@date: 5-14-16
+
+Description: Creates the conflict and broad outlines of its relations
+"""
+
+import #relation
+
+class Conflict:
+
+    self.starting_relation
+
+    self.resolution_relation
+
+    self.final_relation
+
+    self.relation_outlines = [self.starting_relation,
+                              self.resolution_relation,
+                              self.final_relation]
+
+    
