ad3fde91a4e69a4dee44e649e0dc22355818477d
Amol Kapoor <theahura@gmail.com>
2016-05-13 22:35:15 -0400

    Signed-off-by: Amol Kapoor <theahura@gmail.com>

---
+++ source/Author/Cluster.py
@@ -0,0 +1,28 @@
+"""
+@date: 5-13-16
+
+Description: Defines the cluster parent class
+"""
+
+
+class Cluster: 
+    """Defines the cluster parent class, which contains basic methods common to all
+    character, setting, thing clusters"""
+    
+    def __init__(self):
+        pass
+
+    def getName(self):
+        return name
+
+    def find_associated_word(self):
+        pass
+
+    def get_character_relations(self):
+        pass
+
+    def get_setting_relations(self):
+        pass
+
+    def get_thing_relations(self):
+        pass
