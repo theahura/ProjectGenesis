85311638ca6e24482852bd274c81e0b511fb84db
Amol Kapoor <theahura@gmail.com>
2016-05-22 19:12:08 -0700

    Finished story generator (?)
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- source/Author/Story/StoryStructure.py
+++ source/Author/Story/StoryStructure.py
@@ -9,9 +9,11 @@ and its own relations to settings/characters/things.
 
 import random
 from source.Author.Relation import Subsection
+from source.Author.Relation import GenericRelation
+
 
 class StoryStructure:
-    
+
     def __init__(self, settings, conflict):
         """
         Selects a random setting from the list of given settings and uses the conflict to create
@@ -69,15 +71,42 @@ class StoryStructure:
 
         return detail_list
 
-    def generate_generic_relation(protag, setting):
+    def generate_generic_relation(self, protag, setting):
+        """
+        Creates an undefined relation between the protagonist and a thing or character
+        """
         cluster2_list = list(set().union(setting.characters, setting.things, protag.things))
         return GenericRelation(protag, random.choice(cluster2))
 
     def generate_setting_map(self, current_setting, next_setting):
         """ 
         Creates a path from current_setting to next_setting by iterating over nearby settings. 
-        Implement some search algo here.
         """
-        pass
+        traversed_settings = set()
+        return generate_setting_map_helper(current_setting, next_setting, traversed_settings) 
+
+    def generate_setting_map_helper(self, current_node, final_node, traversed_settings):
+        """ 
+        Takes current node, selects a neighbor node that hasn't already been traversed. 
+        Randomly decides if it wants to stay at neighbor node or move one level back.
+        Marks nodes that have been traversed in passed in dictionary.
+        Dictionary stores in form {currentnode.name-nextnode.name} for uniqueness
+        Continued until final_node is reached. Assumes there is a possible path.
+
+        Note: can never return immediately to same location. Could be a problem for max dynamic
+        story telling.
+        """
+
+        if current_node.name == final_node.name:
+            return [current_node]
+        elif: 
+            possible_next_nodes = [x for x in current_node.settings if current_node.name + "-" + x.name not in traversed_settings]
+
+            if not possible_next_nodes:
+                return [current_node]
+            
+            next_node = random.choice(possible_next_nodes)
+
+            return [current_node] + generate_setting_map_helper(next_node, final_node, traversed_settings)
 
 
